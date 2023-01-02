28c4691ce03cde64cc03c28e6aea6d7071f08ba7
Saber Ghasempour <saber.ghasempour@gmail.com>
2023-01-02 15:47:24 +0300

    Styling and Add Skeleton for loading process

--- src/features/category/Header.tsx
+++ src/features/category/Header.tsx
@@ -1,5 +1,5 @@
 import { BellOutlined, ShoppingCartOutlined } from "@ant-design/icons";
-import { Col, Row, Image, Badge } from "antd";
+import { Col, Row, Image, Badge, Skeleton } from "antd";
 import { observer } from "mobx-react-lite";
 
 interface Props {
@@ -11,14 +11,22 @@ export default observer(function Header({ restaurantName, logo }: Props) {
   return (
     <Row align="middle">
       <Col span={4}>
-        <Image
-          src={`https://coffeemenu.ir${logo}`}
-          className="circle-image"
-          preview={false}
-        />
+        {restaurantName == undefined ? (
+          <Skeleton.Avatar active />
+        ) : (
+          <Image
+            src={`https://coffeemenu.ir${logo}`}
+            className="circle-image"
+            preview={false}
+          />
+        )}
       </Col>
       <Col span={12}>
-        <h1>{restaurantName}</h1>
+        {restaurantName == undefined ? (
+          <Skeleton.Input size="small" active />
+        ) : (
+          <h1>{restaurantName}</h1>
+        )}
       </Col>
       {/* <Col span={8} className="title-icons">
         <BellOutlined className="header-icon" />
